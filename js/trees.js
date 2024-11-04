be97e9234f3a947f0b1b25f7d7445d3fffe77c4c
profJoey <137945663+profJoey@users.noreply.github.com>
2024-11-04 10:42:42 -0500

    Add files via upload

---
+++ js/trees.js
@@ -0,0 +1,56 @@
+const trees = [
+    { 
+        name: "Oak",
+        description: "Large, strong tree with lobed leaves.",
+        imagePath: "images/oak.jpg",
+        link: "trees/oak.html" 
+    },
+    { 
+        name: "Pine", 
+        description: "Evergreen tree with needle-like leaves.", 
+        imagePath: "images/pine.jpg", 
+        link: "trees/pine.html" 
+    },
+    { 
+        name: "Maple", 
+        description: "Tree known for its lobed leaves and sap.", 
+        imagePath: "images/maple.jpg", 
+        link: "trees/maple.html" 
+    },
+    { 
+        name: "Birch", 
+        description: "Tree with distinctive white bark.", 
+        imagePath: "images/birch.jpg", 
+        link: "trees/birch.html" 
+    },
+    { 
+        name: "Cherry Blossom", 
+        description: "Tree known for its beautiful pink flowers.", 
+        imagePath: "images/cherry_blossom.jpg", 
+        link: "trees/cherry_blossom.html" 
+    },
+    { 
+        name: "Willow", 
+        description: "Tree with long, drooping branches.", 
+        imagePath: "images/willow.jpg", 
+        link: "trees/willow.html" 
+    },
+    { 
+        name: "Redwood", 
+        description: "Tallest tree species in the world.", 
+        imagePath: "images/redwood.jpg",
+        link: "trees/redwood.html"
+     },
+    { 
+        ame: "Cedar", 
+        description: "Tree with aromatic wood and needle-like leaves.", 
+        imagePath: "images/cedar.jpg", 
+        link: "trees/cedar.html" 
+    },
+    { 
+        name: "Palm", 
+        description: "Tropical tree with large, fan-shaped leaves.", 
+        imagePath: "images/palm.jpg", 
+        link: "trees/palm.html" 
+    }
+];
